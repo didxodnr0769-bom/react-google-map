b00ecc9603ab27408ebe1240d521ab51b1ffb74d
didxodnr0769-bom <54312928+didxodnr0769-bom@users.noreply.github.com>
2025-09-08 16:19:02 +0900

    feat : 기본 구글 맵 연동 코드 작성

---
+++ src/pages/map/MapPage.jsx
@@ -0,0 +1,26 @@
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "100%",
+  height: "400px",
+};
+
+const center = {
+  lat: 37.5665, // 서울의 위도
+  lng: 126.978, // 서울의 경도
+};
+
+const MapPage = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Google Maps with React</h1>
+      <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAP_KEY}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          {/* 마커나 다른 컴포넌트들을 여기에 추가할 수 있습니다 */}
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
+
+export default MapPage;
